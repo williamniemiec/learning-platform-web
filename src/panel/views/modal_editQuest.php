6c8816004f6d6b4712c1898673bd7bc68f499fbc
William Niemiec <williamniemiec@hotmail.com>
2020-05-02 10:37:31 -0300

    Edit class

---
+++ src/panel/views/modal_editQuest.php
@@ -0,0 +1,35 @@
+<div id="modal_editQuest" class="modal fade" role="alert">
+	<div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h3 class="modal-title">Edit class</h3>
+				<button class="close" data-dismiss="modal">&times;</button>
+			</div>
+			<div class="modal-body">
+				<div class="form-group">
+					<label for="edit_classType_quest_name">Question name</label>
+					<input id="edit_classType_quest_name" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label>Questions</label>
+					<input id="edit_classType_quest_q1" class="form-control" placeholder="Question" /><br />
+					<input id="edit_classType_quest_q2" class="form-control" placeholder="Question" /><br />
+					<input id="edit_classType_quest_q3" class="form-control" placeholder="Question" /><br />
+					<input id="edit_classType_quest_q4" class="form-control" placeholder="Question" /><br />
+				</div>
+				<div class="form-group">
+					<label for="edit_answer">Answer</label>
+					<select id="edit_answer" class="form-control">
+						<option value="1">Question 1</option>
+						<option value="2">Question 2</option>
+						<option value="3">Question 3</option>
+						<option value="4">Question 4</option>
+					</select>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-primary btn-block" onclick="editQuest(this)">Edit</button>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/panel/views/modal_editVideo.php
@@ -0,0 +1,27 @@
+<div id="modal_editVideo" class="modal fade" role="alert">
+	<div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h3 class="modal-title">Edit class</h3>
+				<button class="close" data-dismiss="modal">&times;</button>
+			</div>
+			<div class="modal-body">
+				<div class="form-group">
+					<label for="edit_classType_video_title">Title</label>
+					<input id="edit_classType_video_title" class="form-control" />
+				</div>
+				<div class="form-group">
+					<label for="edit_classType_video_description">Description</label>
+					<textarea class="form-control" id="edit_classType_video_description"></textarea>
+				</div>
+				<div class="form-group">
+					<label for="edit_classType_video_url">Youtube URL</label>
+					<input id="edit_classType_video_url" class="form-control" />
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-primary btn-block" onclick="editVideo(this)">Add</button>
+			</div>
+		</div>
+	</div>
+</div>
