da2051a6aad481415afea92d84e81e0c71ee1b4c
William Niemiec <williamniemiec@hotmail.com>
2020-06-08 20:26:47 -0300

    Class - new design

--- src/views/course_left.php
+++ src/views/course_left.php
@@ -2,18 +2,21 @@
 	<div class="module">
 		<h4><?php echo $module['name']; ?></h4>
 		<div class="module_classes">
+			<?php $id = 0; ?>
 			<?php foreach ($module['classes'] as $class): ?>
 				<div class="module_class">
 					<?php if ($class['type'] == 'video'): ?>
-						<a href="<?php echo BASE_URL."courses/class/".$class['id']; ?>">
-							<?php echo $class['video']['title']; ?>
-						</a>
+						<div class="module_title">
+    						<a href="<?php echo BASE_URL."courses/class/".$class['id']; ?>">
+    							<?php echo ++$id.". ".$class['video']['title']; ?>
+    						</a>
+						</div>
 						<?php if ($class['watched'] > 0): ?>
 							<small class="class_watched">Watched</small>
 						<?php endif; ?>
 					<?php else: ?>
 						<a href="<?php echo BASE_URL."courses/class/".$class['id']; ?>">
-							Questionnaire
+							<?php echo ++$id.". Questionnaire"; ?>
 						</a>
 						<?php if ($class['watched'] > 0): ?>
 							<small class="class_watched">Watched</small>
