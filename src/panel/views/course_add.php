e85da5c95b6b4f70f29d56bf38c5792ae3847127
William Niemiec <williamniemiec@hotmail.com>
2020-06-11 22:09:37 -0300

    Admin area - course add - new visual

--- src/panel/views/course_add.php
+++ src/panel/views/course_add.php
@@ -1,32 +1,35 @@
 <div class="container">
-    <?php if ($error): ?>
-    	<div class="alert alert-danger fade show" role="alert">
-    		<button class="close" data-dismiss="alert" aria-label="close">
-    			<span aria-hidden="true">&times;</span>
-    		</button>
-    		<h4 class="alert-heading">Error</h4>
-    		<?php echo $msg; ?>
-    	</div>
-    <?php endif; ?>
-    
-    <form method="POST" enctype="multipart/form-data">
-    	<div class="form-group">
-    		<label for="name">Course name</label>
-    		<input id="name" type="text" name="name" placeholder="Name" class="form-control" required />
-    	</div>
-    	
-    	<div class="form-group">
-    		<label for="description">Description name</label>
-    		<textarea id="description" name="description" class="form-control"></textarea>
-    	</div>
-    	
-    	<div class="form-group">
-    		<label for="logo">Logo</label>
-    		<input id="logo" name="logo" type="file" accept=".jpeg,.png,.jpg" class="form-control" />
-    	</div>
-    	
-    	<div class="form-group">
-    		<input type="submit" value="Register" class="form-control" />
-    	</div>
-    </form>
+	<div class="view_content theme_light">
+		<h1>New course</h1>
+        <?php if ($error): ?>
+        	<div class="alert alert-danger fade show" role="alert">
+        		<button class="close" data-dismiss="alert" aria-label="close">
+        			<span aria-hidden="true">&times;</span>
+        		</button>
+        		<h4 class="alert-heading">Error</h4>
+        		<?php echo $msg; ?>
+        	</div>
+        <?php endif; ?>
+        
+        <form method="POST" enctype="multipart/form-data">
+        	<div class="form-group">
+        		<label for="name">Course name</label>
+        		<input id="name" type="text" name="name" placeholder="Name" class="form-control" required />
+        	</div>
+        	
+        	<div class="form-group">
+        		<label for="description">Description name</label>
+        		<textarea id="description" name="description" class="form-control"></textarea>
+        	</div>
+        	
+        	<div class="form-group">
+        		<label for="logo">Logo</label>
+        		<input id="logo" name="logo" type="file" accept=".jpeg,.png,.jpg" class="form-control" />
+        	</div>
+        	
+        	<div class="form-group">
+        		<input type="submit" value="Register" class="form-control" />
+        	</div>
+        </form>
+    </div>
 </div>
