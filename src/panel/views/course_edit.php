c5a09ddda558f2520442e808894e0416b7660542
William Niemiec <williamniemiec@hotmail.com>
2020-06-12 20:29:38 -0300

    Admin area - View content - new template

--- src/panel/views/course_edit.php
+++ src/panel/views/course_edit.php
@@ -1,82 +1,84 @@
 <div class="container">
-	<div class="view_content theme_light">
-        <?php if ($error): ?>
-        	<div class="alert alert-danger fade show" role="alert">
-        		<button class="close" data-dismiss="alert" aria-label="close">
-        			<span aria-hidden="true">&times;</span>
-        		</button>
-        		<h4 class="alert-heading">Error</h4>
-        		<?php echo $msg; ?>
-        	</div>
-        <?php endif; ?>
-        
-        <h1>Course editing</h1>
-        <h2>Course info</h2>
-        
-        <form method="POST" enctype="multipart/form-data">
-        	<div class="form-group">
-        		<label for="name">Course name</label>
-        		<input id="name" type="text" name="name" placeholder="Name" class="form-control" required value="<?php echo $course['name']; ?>" />
-        	</div>
-        	
-        	<div class="form-group">
-        		<label for="description">Description name</label>
-        		<textarea id="description" name="description" class="form-control"><?php echo $course['description']; ?></textarea>
-        	</div>
-        	
-        	<div class="form-group">
-        		<label for="logo">Current logo</label><br />
-        		<img class="img img-responsive img-thumbnail img_courseEdit" src="<?php echo BASE_URL."../assets/images/logos/".$course['logo']; ?>" /><br /><br />
-        		<input id="logo" name="logo" type="file" accept=".jpeg,.png,.jpg" class="form-control" />
-        	</div>
-        	
-        	<div class="form-group">
-        		<input class="btn_theme" type="submit" value="Save" class="form-control" />
-        	</div>
-        </form>
-        
-        <hr />
-        
-        <div class="modules" data-id_course = "<?php echo $course['id']; ?>">
-        	<h2>Modules</h2>
-        	<button class="btn_theme" data-toggle="modal" data-target="#addModule">Add module</button>
-        	
-        	<?php foreach ($modules as $module): ?>
-        		<div class="module" data-id_module="<?php echo $module['id']; ?>">
-        			<div class="module_actions">
-            			<button class="btn_theme" onclick="show_addClass(this,<?php echo $module['id']; ?>)">Add class</button>
-            			<button class="btn_theme" onclick="show_editModule(this,<?php echo $module['id']; ?>)">Edit Module</button>
-            			<button class="btn_theme btn_theme_danger" onclick="deleteModule(this,<?php echo $module['id']; ?>)">Delete module</button>
-        			</div>
-            		<h3 class="moduleName"><?php echo $module['name']; ?></h3>
-            		<div class="classes">
-            			<?php foreach ($module['classes'] as $class): ?>
-                			<div class="class">
-                				<?php if ($class['type'] == 'video'): ?>
-                    				<h5 class="class_title" data-id_video="<?php echo $class['video']['id']; ?>"><?php echo $class['video']['title']; ?></h5>
-                    				<div class="class_actions">
-                    					<button class="btn_theme" onclick="show_editVideo(this,<?php echo $class['video']['id']; ?>)">Edit</button>
-                    					<button class="btn_theme btn_theme_danger" onclick="deleteClass(this,<?php echo $class['id']; ?>)">Delete</button>
-                					</div>
-                				<?php else: ?>
-                					<h5 class="class_title" data-id_quest="<?php echo $class['quest']['id']; ?>"><?php echo $class['quest']['question']; ?></h5>
-                					<div class="class_actions">
-                    					<button class="btn_theme" onclick="show_editQuest(this,<?php echo $class['quest']['id']; ?>)">Edit</button>
-                    					<button class="btn_theme btn_theme_danger" onclick="deleteClass(this,<?php echo $class['id']; ?>)">Delete</button>
-                					</div>
-                				<?php endif; ?>
-                			</div>
-            			<?php endforeach; ?>
+	<div class="theme_light">
+		<h1 class="view_header">Course editing</h1>
+		<div class="view_content">
+            <?php if ($error): ?>
+            	<div class="alert alert-danger fade show" role="alert">
+            		<button class="close" data-dismiss="alert" aria-label="close">
+            			<span aria-hidden="true">&times;</span>
+            		</button>
+            		<h4 class="alert-heading">Error</h4>
+            		<?php echo $msg; ?>
+            	</div>
+            <?php endif; ?>
+           
+            <h2>Course info</h2>
+            
+            <form method="POST" enctype="multipart/form-data">
+            	<div class="form-group">
+            		<label for="name">Course name</label>
+            		<input id="name" type="text" name="name" placeholder="Name" class="form-control" required value="<?php echo $course['name']; ?>" />
+            	</div>
+            	
+            	<div class="form-group">
+            		<label for="description">Description name</label>
+            		<textarea id="description" name="description" class="form-control"><?php echo $course['description']; ?></textarea>
+            	</div>
+            	
+            	<div class="form-group">
+            		<label for="logo">Current logo</label><br />
+            		<img class="img img-responsive img-thumbnail img_courseEdit" src="<?php echo BASE_URL."../assets/images/logos/".$course['logo']; ?>" /><br /><br />
+            		<input id="logo" name="logo" type="file" accept=".jpeg,.png,.jpg" class="form-control" />
+            	</div>
+            	
+            	<div class="form-group">
+            		<input class="btn_theme" type="submit" value="Save" class="form-control" />
+            	</div>
+            </form>
+            
+            <hr />
+            
+            <div class="modules" data-id_course = "<?php echo $course['id']; ?>">
+            	<h2>Modules</h2>
+            	<button class="btn_theme" data-toggle="modal" data-target="#addModule">Add module</button>
+            	
+            	<?php foreach ($modules as $module): ?>
+            		<div class="module" data-id_module="<?php echo $module['id']; ?>">
+            			<div class="module_actions">
+                			<button class="btn_theme" onclick="show_addClass(this,<?php echo $module['id']; ?>)">Add class</button>
+                			<button class="btn_theme" onclick="show_editModule(this,<?php echo $module['id']; ?>)">Edit Module</button>
+                			<button class="btn_theme btn_theme_danger" onclick="deleteModule(this,<?php echo $module['id']; ?>)">Delete module</button>
+            			</div>
+                		<h3 class="moduleName"><?php echo $module['name']; ?></h3>
+                		<div class="classes">
+                			<?php foreach ($module['classes'] as $class): ?>
+                    			<div class="class">
+                    				<?php if ($class['type'] == 'video'): ?>
+                        				<h5 class="class_title" data-id_video="<?php echo $class['video']['id']; ?>"><?php echo $class['video']['title']; ?></h5>
+                        				<div class="class_actions">
+                        					<button class="btn_theme" onclick="show_editVideo(this,<?php echo $class['video']['id']; ?>)">Edit</button>
+                        					<button class="btn_theme btn_theme_danger" onclick="deleteClass(this,<?php echo $class['id']; ?>)">Delete</button>
+                    					</div>
+                    				<?php else: ?>
+                    					<h5 class="class_title" data-id_quest="<?php echo $class['quest']['id']; ?>"><?php echo $class['quest']['question']; ?></h5>
+                    					<div class="class_actions">
+                        					<button class="btn_theme" onclick="show_editQuest(this,<?php echo $class['quest']['id']; ?>)">Edit</button>
+                        					<button class="btn_theme btn_theme_danger" onclick="deleteClass(this,<?php echo $class['id']; ?>)">Delete</button>
+                    					</div>
+                    				<?php endif; ?>
+                    			</div>
+                			<?php endforeach; ?>
+                		</div>
             		</div>
-        		</div>
-    		<?php endforeach; ?>
-        </div>
-        
-        <!-- Modals -->
-    	<?php $this->loadView("modal_addModule"); ?>
-    	<?php $this->loadView("modal_editModule"); ?>
-    	<?php $this->loadView("modal_addClass"); ?>
-    	<?php $this->loadView("modal_editVideo"); ?>
-    	<?php $this->loadView("modal_editQuest"); ?>
+        		<?php endforeach; ?>
+            </div>
+            
+            <!-- Modals -->
+        	<?php $this->loadView("modal_addModule"); ?>
+        	<?php $this->loadView("modal_editModule"); ?>
+        	<?php $this->loadView("modal_addClass"); ?>
+        	<?php $this->loadView("modal_editVideo"); ?>
+        	<?php $this->loadView("modal_editQuest"); ?>
+    	</div>
 	</div>
 </div>
--- src/panel/views/home.php
+++ src/panel/views/home.php
@@ -1,38 +1,41 @@
 <div class="container">
-	<div class="view_content">
-    <h1>Courses</h1>
-    <?php if (count($courses) == 0): ?>
-    	<h2>There are not registered courses for this account :/</h2>
-    <?php else: ?>
-    	<a class="btn_theme" href="<?php BASE_URL; ?>courses/add">Add</a>
-        <table class="table table-hover table-stripped table-bordered">
-        	<thead>
-        		<tr>
-        			<th></th>
-            		<th>Name</th>
-            		<th>Description</th>
-            		<th>Students</th>
-            		<th>Actions</th>
-        		</tr>
-        	</thead>
-        	<tbody>
-        		<?php foreach($courses as $course): ?>
-            		<tr>
-            			<?php if (empty($course['logo'])): ?>
-            				<td class="course_logo"><img class="img img-responsive" src="<?php echo BASE_URL."../assets/images/noImage"; ?>" /></td>
-            			<?php else: ?>
-            				<td class="course_logo"><img class="img img-responsive" src="<?php echo BASE_URL."../assets/images/logos/".$course['logo']; ?>" /></td>
-            			<?php endif; ?>
-            			<td><a href="<?php echo BASE_URL."courses/edit/".$course['id']; ?>"><?php echo $course['name']; ?></a></td>
-            			<td><?php echo $course['description']; ?></td>
-            			<td><?php echo $course['total_students']; ?></td>
-            			<td>
-            				<a class="btn btn-warning" href="<?php echo BASE_URL."courses/edit/".$course['id']; ?>">Edit</a>
-            				<a class="btn btn-danger" href="<?php echo BASE_URL."courses/delete/".$course['id']; ?>">Delete</a>
-        				</td>
-            		</tr>
-        		<?php endforeach; ?>
-        	</tbody>
-        </table>
-    <?php endif; ?>
-</div></div>
+	<div class="theme_light">
+    	<h1 class="view_header">Courses</h1>
+    	<div class="view_content">
+            <?php if (count($courses) == 0): ?>
+            	<h2>There are not registered courses for this account :/</h2>
+            <?php else: ?>
+            	<a class="btn_theme" href="<?php BASE_URL; ?>courses/add">Add</a>
+                <table class="table table-hover table-stripped text_centered">
+                	<thead>
+                		<tr>
+                			<th></th>
+                    		<th>Name</th>
+                    		<th>Description</th>
+                    		<th>Students</th>
+                    		<th>Actions</th>
+                		</tr>
+                	</thead>
+                	<tbody>
+                		<?php foreach($courses as $course): ?>
+                    		<tr>
+                    			<?php if (empty($course['logo'])): ?>
+                    				<td class="course_logo"><img class="img img-responsive" src="<?php echo BASE_URL."../assets/images/noImage"; ?>" /></td>
+                    			<?php else: ?>
+                    				<td class="course_logo"><img class="img img-responsive" src="<?php echo BASE_URL."../assets/images/logos/".$course['logo']; ?>" /></td>
+                    			<?php endif; ?>
+                    			<td><a href="<?php echo BASE_URL."courses/edit/".$course['id']; ?>"><?php echo $course['name']; ?></a></td>
+                    			<td><?php echo $course['description']; ?></td>
+                    			<td><?php echo $course['total_students']; ?></td>
+                    			<td class="actions">
+                    				<a class="btn_theme" href="<?php echo BASE_URL."courses/edit/".$course['id']; ?>">Edit</a>
+                    				<a class="btn_theme btn_theme_danger" href="<?php echo BASE_URL."courses/delete/".$course['id']; ?>">Delete</a>
+                				</td>
+                    		</tr>
+                		<?php endforeach; ?>
+                	</tbody>
+                </table>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
