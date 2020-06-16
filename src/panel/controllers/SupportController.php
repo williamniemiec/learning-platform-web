11eab079d6bc08412266f5a2cee6fac366eebcc5
William Niemiec <williamniemiec@hotmail.com>
2020-06-16 18:39:38 -0300

    SupportController - doc

--- src/panel/controllers/SupportController.php
+++ src/panel/controllers/SupportController.php
@@ -8,13 +8,21 @@ use models\Courses;
 
 
 /**
-
-*/
+ * Responsible for the behavior of the view {@link support/support.php}.
+ *
+ * @author		William Niemiec &lt; williamniemiec@hotmail.com &gt;
+ * @version		1.0
+ * @since		1.0
+ */
 class SupportController extends Controller
 {
     //-----------------------------------------------------------------------
     //        Constructor
     //-----------------------------------------------------------------------
+    /**
+    * It will check if admin is logged; otherwise, redirects him to login
+    * page.
+    */
     public function __construct()
     {
         if (!Students::isLogged() && !Admins::isLogged()){
@@ -36,41 +44,36 @@ class SupportController extends Controller
         $courses = new Courses();
         
         $header = array(
-            'title' => 'Learning platform - Support',
-            'styles' => array('support', 'style')
-            //'description' => "A website made using MVC-in-PHP framework",
-            //'keywords' => array('home', 'mvc-in-php'),
-            //'robots' => 'index'
+            'title' => 'Support - Learning platform',
+            'styles' => array('support')
         );
         
         $params = array(
             'adminName' => $admins->getName(),
             'courses' => $courses->getCourses(),
-            'header' => $header,
-            'scripts' => array()
+            'header' => $header
         );
         
         $this->loadTemplate("support/support", $params);
     }
     
+    /**
+     * Opens a topic from support.
+     */
     public function open()
     {
         $admins = new Admins($_SESSION['a_login']);
         $courses = new Courses();
         
         $header = array(
-            'title' => 'Learning platform - Support',
-            'styles' => array('support', 'style')
-            //'description' => "A website made using MVC-in-PHP framework",
-            //'keywords' => array('home', 'mvc-in-php'),
-            //'robots' => 'index'
+            'title' => 'Support - Learning platform',
+            'styles' => array('support')
         );
         
         $params = array(
             'adminName' => $admins->getName(),
             'courses' => $courses->getCourses(),
-            'header' => $header,
-            'scripts' => array()
+            'header' => $header
         );
         
         $this->loadTemplate("supports/support_content", $params);
