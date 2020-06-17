3b7f879a57e027e47e5dc28a63cce0dba148ce3e
William Niemiec <williamniemiec@hotmail.com>
2020-06-16 21:19:45 -0300

    Admin area - Admins model doc

--- src/panel/models/Admins.php
+++ src/panel/models/Admins.php
@@ -5,19 +5,25 @@ use core\Model;
 
 
 /**
- *
+ * Responsible for managing admins.
  */
 class Admins extends Model
 {
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     //        Attributes
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     private $id_user;
     
     
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     //        Constructor
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    /**
+     * Creates admins manager.
+     * 
+     * @param       int $id_user [Optional] Admin id
+     * @apiNote     It will connect to the database when it is instantiated
+     */
     public function __construct($id_user = -1)
     {
         parent::__construct();
@@ -25,9 +31,14 @@ class Admins extends Model
     }
     
     
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     //        Methods
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    /**
+     * Checks whether current user is logged.
+     * 
+     * @return      boolean Whether current user is logged
+     */
     public static function isLogged()
     {
         if (empty($_SESSION['a_login'])) {
@@ -37,9 +48,16 @@ class Admins extends Model
         return true;
     }
     
+    /**
+     * Checks whether the supplied credentials are valid.
+     * 
+     * @param       string $email Admin email
+     * @param       string $pass Admin password
+     * @return      boolean If credentials are correct
+     */
     public function login($email, $pass)
     {
-        if (empty($email) || empty($pass)) { return false; }
+        if (empty($email) || empty($pass))  { return false; }
         
         $sql = $this->db->prepare("SELECT id FROM admins WHERE email = ? AND password = ?");
         $sql->execute(array($email, md5($pass)));
@@ -52,9 +70,15 @@ class Admins extends Model
         return true;
     }
     
+    /**
+     * Gets name from logged admin.
+     * 
+     * @return      string Admin name
+     */
     public function getName()
     {
         $sql = $this->db->query("SELECT name FROM admins WHERE id = $this->id_user");
+        
         return $sql->fetch()['name'];
     }
 }
