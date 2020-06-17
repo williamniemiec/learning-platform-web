e3713b132728798b2fd0d8f2c88a0a2167169ff9
William Niemiec <williamniemiec@hotmail.com>
2020-06-16 21:20:00 -0300

    Admin area - Classes model doc

--- src/panel/models/Classes.php
+++ src/panel/models/Classes.php
@@ -5,22 +5,33 @@ use core\Model;
 
 
 /**
- *
+ * Responsible for managing classes.
  */
 class Classes extends Model
 {
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     //        Constructor
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    /**
+     * Creates classes manager.
+     *
+     * @apiNote     It will connect to the database when it is instantiated
+     */
     public function __construct()
     {
         parent::__construct();
     }
     
     
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
     //        Methods
-    //-----------------------------------------------------------------------
+    //-------------------------------------------------------------------------
+    /**
+     * Gets total of classes from a course.
+     * 
+     * @param       int $id_course Course id
+     * @return      int Total course classes
+     */
     public function countClasses($id_course)
     {
         if (empty($id_course) || $id_course <= 0) { return 0; }
@@ -30,12 +41,18 @@ class Classes extends Model
         return $sql->fetch()['count'];
     }
     
+    /**
+     * Gets informations about all classes from a module.
+     * 
+     * @param       int $id_module Module id
+     * @return      array Informations about all classes from the module
+     */
     public function getClassesFromModule($id_module)
     {
         if (empty($id_module) || $id_module <= 0) { return array(); }
         
         $response = array();
-       // $id_student = $_SESSION['s_login'];
+        
         $sql = $this->db->prepare("
             SELECT 
                 * 
@@ -43,6 +60,7 @@ class Classes extends Model
             WHERE id_module = ? 
             ORDER BY classes.order
         ");
+        
         $sql->execute(array($id_module));
         
         if ($sql->rowCount() > 0) {
@@ -62,6 +80,12 @@ class Classes extends Model
         return $response;
     }
     
+    /**
+     * Gets course id from a class.
+     * 
+     * @param       int $id_class
+     * @return      number Course id of -1 if class id is invalid
+     */
     public function getCourseId($id_class)
     {
         if (empty($id_class) || $id_class <= 0) { return -1; }
@@ -72,6 +96,14 @@ class Classes extends Model
         return $sql->fetch(\PDO::FETCH_ASSOC)['id_course'];
     }
     
+    /**
+     * Gets information about the first class from the first module from a
+     * course.
+     * 
+     * @param       int $id_course Course id
+     * @return      array Informations about the first class from the first
+     * module from a course
+     */
     public function getFirstClassFromFirstModule($id_course)
     {
         $response = array();
@@ -85,6 +117,7 @@ class Classes extends Model
             ORDER BY id_module ASC 
             LIMIT 1
         ");
+        
         $sql->execute(array($id_course));
         
         if ($sql->rowCount() > 0) {
@@ -102,37 +135,12 @@ class Classes extends Model
         return $response;
     }
     
-    public function getClass($id_class, $id_student)
-    {
-        if (empty($id_class) || $id_class <= 0) { return -1; }
-        
-        $response = array();
-        
-        $sql = $this->db->prepare("
-            SELECT 
-                *,
-                (select count(*) from historic where historic.id_class = classes.id and historic.id_student = $id_student) as watched
-            FROM classes 
-            WHERE id = ?
-        ");
-        $sql->execute(array($id_class));
-        
-        if ($sql->rowCount() > 0) {
-            $response = $sql->fetch(\PDO::FETCH_ASSOC);
-            
-            if ($response['type'] == 'video') {
-                $videos = new Videos();
-                $response['video'] = $videos->getVideoFromClass($id_class);
-            } else {
-                $quests = new Questionnaires();
-                $response['quest'] = $quests->getQuestFromClass($id_class);
-            }
-        }
-        
-        return $response; 
-        
-    }
-    
+    /**
+     * Checks whether there is a class with the given id.
+     * 
+     * @param       int $id_class Class id
+     * @return      boolean If there is a class with the given id
+     */
     public function exist($id_class)
     {
         if (empty($id_class) || $id_class <= 0) { return false; }
@@ -143,27 +151,15 @@ class Classes extends Model
         return $sql->fetch()['count'] > 0;
     }
     
-    public function markAsWatched($id_student,$id_class)
-    {
-        if (empty($id_class) || $id_class <= 0) { return; }
-        
-        if ($this->alreadyMarkedAsWatched($id_student,$id_class)) { return; }
-        
-        $sql = $this->db->prepare("INSERT INTO historic (id_student,id_class,date_watched) VALUES (?,?,NOW())");
-        $sql->execute(array($id_student,$id_class));   
-    }
-    
-    public function removeWatched($id_student,$id_class)
-    {
-        if (empty($id_class) || $id_class <= 0) { return; }
-        
-        $sql = $this->db->prepare("DELETE FROM historic WHERE id_student = ? AND id_class = ?");
-        $sql->execute(array($id_student,$id_class));
-    }
-    
+    /**
+     * Gets all classes from a course.
+     * 
+     * @param       int $id_course Course id
+     * @return      array Informations about all classes from a course
+     */
     public function getClassesInCourse($id_course)
     {
-        if (empty($id_course) || $id_course <= 0) { return; }
+        if (empty($id_course) || $id_course <= 0) { return array(); }
         
         $response = array();
         
@@ -179,6 +175,11 @@ class Classes extends Model
         return $response;
     }
     
+    /**
+     * Deletes a class from a course.
+     * 
+     * @param       int $id_class Class id
+     */
     public function delete($id_class)
     {
         if (empty($id_class) || $id_class <= 0) { return; }
@@ -204,6 +205,15 @@ class Classes extends Model
         $sql->execute(array($id_class));
     }
     
+    /**
+     * Adds a class to a course
+     * 
+     * @param       int $id_module Module id
+     * @param       int $id_course Course id
+     * @param       int $type Class type ('video' or 'quest')
+     * @param       int $order [Optional] Class order within a course module
+     * @return      int Id of added class or -1 if any argument is invalid
+     */
     public function add($id_module, $id_course, $type, $order=0)
     {
         if (empty($id_module) || $id_module <= 0) { return -1; }
@@ -229,11 +239,26 @@ class Classes extends Model
         return $response;
     }
     
+    /**
+     * Gets class with higher order.
+     * 
+     * @param       int $id_module Module id
+     * @param       int $id_course Course id
+     * @return      int Class id with higher order or zero if there is no class
+     * registered.
+     */
     private function getLastOrder($id_module, $id_course)
     {
         $response = 0;
         
-        $sql = $this->db->prepare("SELECT classes.order FROM classes WHERE id_module = ? AND id_course = ? ORDER BY classes.order DESC LIMIT 1");
+        $sql = $this->db->prepare("
+            SELECT classes.order 
+            FROM classes 
+            WHERE id_module = ? AND id_course = ? 
+            ORDER BY classes.order DESC 
+            LIMIT 1
+        ");
+        
         $sql->execute(array($id_module, $id_course));
         
         if ($sql->rowCount() > 0) {
@@ -241,16 +266,5 @@ class Classes extends Model
         }
 
         return $response;
-    }
-    
-    private function alreadyMarkedAsWatched($id_class, $id_student)
-    {
-        if (empty($id_class) || $id_class <= 0)     { return true; }
-        if (empty($id_student) || $id_student <= 0) { return true; }
-        
-        $sql = $this->db->prepare("SELECT COUNT(*) as count FROM historic WHERE id_student = ? AND id_class = ?");
-        $sql->execute(array($id_student,$id_class));
-        
-        return $sql->fetch()["count"] > 0;
     }
 }
