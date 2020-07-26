a53445503938f2acca1b5c32449fab268ddcbc62
William Niemiec <williamniemiec@hotmail.com>
2020-07-26 12:34:05 -0300

    Notifications model - doc

--- src/models/Notifications.php
+++ src/models/Notifications.php
@@ -1,16 +1,19 @@
 <?php
+declare (strict_types=1);
+
 namespace models;
+
 
 use core\Model;
 use models\obj\Notification;
-
+use models\enum\NotificationTypeEnum;
 
 /**
- * Responsible for managing notifications table.
+ * Responsible for managing 'notifications' table.
  * 
  * @author		William Niemiec &lt; williamniemiec@hotmail.com &gt;
- * @version		1.0
- * @since		1.0
+ * @version		1.0.0
+ * @since		1.0.0
  */
 class Notifications extends Model
 {
@@ -24,11 +27,11 @@ class Notifications extends Model
     //        Constructor
     //-------------------------------------------------------------------------
     /**
-     * Creates notifications table manager.
+     * Creates 'notifications' table manager.
      *
      * @apiNote     It will connect to the database when it is instantiated
      */
-    public function __construct($id_user)
+    public function __construct(int $id_user)
     {
         parent::__construct();
         $this->id_student = $id_user;
@@ -38,36 +41,52 @@ class Notifications extends Model
     //-------------------------------------------------------------------------
     //        Methods
     //-------------------------------------------------------------------------
-    public function getNotifications($limit = -1)
+    /**
+     * Gets notifications from current student.
+     * 
+     * @param       int $limit [Optional] Maximum notifications that will be 
+     * caught. Default value is 10
+     * 
+     * @return      Notification[] Notifications that current student has
+     * 
+     * @throws      \InvalidArgumentException If any argument is invalid 
+     */
+    public function getNotifications(int $limit = 10) : array
     {
+        if (empty($limit) || $limit <= 0)
+            throw new \InvalidArgumentException("Invalid limit");
+        
         $response = array();
         
-        $query = "
+        // Query construction
+        $sql = $this->db->prepare("
             SELECT  *,
             CASE
-               WHEN type = 0 THEN (SELECT   text 
-                                   FROM     comments 
+               WHEN type = 0 THEN (SELECT   text
+                                   FROM     comments
                                    WHERE    id_comment = id_reference)
-               ELSE (SELECT message 
-                     FROM   support_topic 
+               ELSE (SELECT message
+                     FROM   support_topic
                      WHERE  id_topic = id_reference)
             END AS 'text_ref'
             FROM    notifications
-            WHERE   id_student = ".$this->id_student."
-        ";
+            WHERE   id_student = ?
+            LIMIT   ".$limit."
+        ");
         
-        if ($limit > 0)
-            $query .= " LIMIT $limit";
+        // Executes query
+        $sql->execute(array($this->id_student));
         
-        $sql = $this->db->query($query);
-        
-        if ($sql->rowCount() > 0) {
+        // Parses results
+        if ($sql && $sql->rowCount() > 0) {
             $notifications = $sql->fetchAll();
             
             foreach ($notifications as $notification) {
                 $response[] = new Notification(
                     $notification['id_student'], 
                     $notification['date'],
+                    $notification['id_reference'],
+                    new NotificationTypeEnum($notification['type']),
                     $notification['ref_text'],
                     $notification['type'],
                     $notification['message'],
@@ -79,44 +98,65 @@ class Notifications extends Model
         return $response;
     }
 
-    public function countUnreadNotification($limit = -1)
+    /**
+     * Gets total of unread notifications.
+     * 
+     * @return      int Total unread notifications
+     */
+    public function countUnreadNotification() : int
     {
-        $query = "
+        // Query construction
+        $sql = $this->db->prepare("
             SELECT  COUNT(*) AS total_unread
             FROM    notifications
-            WHERE   `read` = 0
-        ";
+            WHERE   id_student = ? AND `read` = 0
+        ");
 
-        if ($limit > 0) {
-            $query .= " LIMIT ".$limit;
-        }
-
-        return $this->db->query($query)->fetch()['total_unread'];
+        // Executes query
+        $sql->execute(array($this->id_student));
+        
+        return $sql->fetch()['total_unread'];
     }
     
-    public function delete($id_student, $date)
+    /**
+     * Deletes a notification
+     * 
+     * @param       string $date Notification creation date
+     * 
+     * @return      bool If notification was sucessfully deleted
+
+     * @throws      \InvalidArgumentException If any argument is invalid 
+     */
+    public function delete(string $date) : bool
     {
-        if (empty($id_student) || $id_student <= 0)
-            throw new \InvalidArgumentException("Invalid id_student");
-        
         if (empty($date))
             throw new \InvalidArgumentException("Invalid date");
         
+        // Query construction
         $sql = $this->db->prepare("
             DELETE FROM notifications
             WHERE id_student = ? AND date = ?
         ");
         
-        $sql->execute(array($id_student, $date));
+        // Executes query
+        $sql->execute(array($this->id_student, $date));
         
-        return $sql->rowCount() > 0;
+        return $sql && $sql->rowCount() > 0;
     }
     
-    public function new($id_student, $id_reference, $type, $message)
+    /**
+     * Creates a new notification.
+     * 
+     * @param       int $id_reference Reference id (comment id or support topic id)
+     * @param       NotificationTypeEnum $type Notification type
+     * @param       string $message Notification content
+     * 
+     * @return      bool If the notification was sucessfully created
+     * 
+     * @throws \InvalidArgumentException If any argument is invalid 
+     */
+    public function new(int $id_reference, NotificationTypeEnum $type, string $message) : bool
     {
-        if (empty($id_student) || $id_student <= 0)
-            throw new \InvalidArgumentException("Invalid id_student");
-        
         if (empty($id_reference) || $id_reference <= 0)
             throw new \InvalidArgumentException("Invalid id_reference");
         
@@ -126,31 +166,62 @@ class Notifications extends Model
         if (empty($message))
             throw new \InvalidArgumentException("Message cannot be empty");
         
+        // Query construction
         $sql = $this->db->prepare("
             INSERT INTO notifications
             (id_student, date, id_reference, type, message)
             VALUES (?, NOW(), ?, ?, ?)
         ");
         
-        $sql->execute(array($id_student, $id_reference, $type, $message));
+        // Executes query
+        $sql->execute(array($this->id_student, $id_reference, $type->get(), $message));
         
-        return $sql->rowCount() > 0;
+        return $sql && $sql->rowCount() > 0;
     }
     
-    public function markAsRead($id_student, $date)
+    /**
+     * Mark a notification as read.
+     * 
+     * @param       string $date Notification creation date
+     * 
+     * @throws      \InvalidArgumentException If any argument is invalid 
+     */
+    public function markAsRead(string $date) : void
     {
-        if (empty($id_student) || $id_student <= 0)
-            throw new \InvalidArgumentException("Invalid id_student");
-            
         if (empty($date))
             throw new \InvalidArgumentException("Invalid date");
             
+        // Query construction
         $sql = $this->db->prepare("
             UPDATE  notifications
             SET     read = 1
             WHERE   id_student = ? AND date = ?
         ");
         
-        $sql->execute(array($id_student, $date));
+        // Executes query
+        $sql->execute(array($this->id_student, $date));
+    }
+    
+    /**
+     * Mark a notification as unread.
+     *
+     * @param       string $date Notification creation date
+     *
+     * @throws      \InvalidArgumentException If any argument is invalid
+     */
+    public function markAsUnread(string $date) : void
+    {
+        if (empty($date))
+            throw new \InvalidArgumentException("Invalid date");
+            
+            // Query construction
+            $sql = $this->db->prepare("
+            UPDATE  notifications
+            SET     read = 0
+            WHERE   id_student = ? AND date = ?
+        ");
+            
+            // Executes query
+            $sql->execute(array($this->id_student, $date));
     }
 }
