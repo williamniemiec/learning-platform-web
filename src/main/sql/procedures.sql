6267ce47ac482e8553513ded12a126424b332d59
William Niemiec <williamniemiec@hotmail.com>
2022-06-10 12:36:58 -0300

    SQL - License added

--- src/main/sql/procedures.sql
+++ src/main/sql/procedures.sql
@@ -1,9 +1,9 @@
-/**
- * Copyright (c) William Niemiec.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+--
+-- Copyright (c) William Niemiec.
+--
+-- This source code is licensed under the MIT license found in the
+-- LICENSE file in the root directory of this source tree.
+--
 
 -- ----------------------------------------------------------------------------
 -- Code developed on MySQL 5.7.26.
